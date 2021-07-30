2f544c85f3c71c47e866bf6661c2d9b84fc22a7d
Armando114 <71297507+Armando114@users.noreply.github.com>
2021-07-30 18:30:12 -0400

    Add files via upload

---
+++ Horario Laravel/resources/views/layouts/horario.blade.php
@@ -0,0 +1,106 @@
+@extends("layouts.plantilla")
+
+
+@section("cabecera")
+
+<h1>Horario</h1>
+
+@endsection
+
+
+@section("tabla")
+
+<table>
+        <tr>
+            <th>Horas</th>
+            <th>Lunes</th>
+            <th>Martes</th>
+            <th>Miercoles</th>
+            <th>Jueves</th>
+            <th>Viernes</th>
+            <th>Sabado</th>
+            <th>Domingo</th>
+        </tr>
+        <tr>
+            <td>08:00 - 09:00</td>
+            <td>Elaborar informes</td>
+            <td>Elaborar informes</td>
+            <td>Elaborar informes</td>
+            <td>Elaborar informes</td>
+            <td>Elaborar informes</td>
+            <td>LIBRE</td>
+            <td>LIBRE</td>
+        </tr>
+        <tr>
+            <td>09:00 - 10:00</td>
+            <td>Gestionar proyectos</td>
+            <td>Gestionar proyectos</td>
+            <td>Gestionar proyectos</td>
+            <td>Gestionar proyectos</td>
+            <td>Reunión</td>
+            <td>LIBRE</td>
+            <td>LIBRE</td>
+        </tr>
+        <tr>
+            <td>10:00 - 11:00</td>
+            <td>Organizar archivos</td>
+            <td>Elaborar informes</td>
+            <td>Organizar archivos</td>
+            <td>Elaborar informes</td>
+            <td>Reunión</td>
+            <td>LIBRE</td>
+            <td>LIBRE</td>
+        </tr>
+        <tr>
+            <td>11:00 - 12:00</td>
+            <td>Elaborar informes</td>
+            <td>Elaborar informes</td>
+            <td>Elaborar informes</td>
+            <td>Elaborar informes</td>
+            <td>Elaborar informes</td>
+            <td>LIBRE</td>
+            <td>LIBRE</td>
+        </tr>
+        <tr>
+            <td>12:00 - 13:00</td>
+            <td>Almuerzo</td>
+            <td>Almuerzo</td>
+            <td>Almuerzo</td>
+            <td>Almuerzo</td>
+            <td>Almuerzo</td>
+            <td>LIBRE</td>
+            <td>LIBRE</td>
+        </tr>
+        <tr>
+            <td>13:00 - 14:00</td>
+            <td>Elaborar informes</td>
+            <td>Reunión</td>
+            <td>Elaborar informes</td>
+            <td>Reunión</td>
+            <td>Organizar archivos</td>
+            <td>LIBRE</td>
+            <td>LIBRE</td>
+        </tr>
+        <tr>
+            <td>14:00 - 15:00</td>
+            <td>Elaborar informes</td>
+            <td>Elaborar informes</td>
+            <td>Elaborar informes</td>
+            <td>Elaborar informes</td>
+            <td>Gestionar proyectos</td>
+            <td>LIBRE</td>
+            <td>LIBRE</td>
+        </tr>
+        <tr>
+            <td>15:00 - 16:00</td>
+            <td>Organizar archivos</td>
+            <td>Organizar archivos</td>
+            <td>Organizar archivos</td>
+            <td>Organizar archivos</td>
+            <td>Gestionar proyectos</td>
+            <td>LIBRE</td>
+            <td>LIBRE</td>
+        </tr>
+    </table>
+
+@endsection
---
+++ Horario Laravel/resources/views/layouts/plantilla.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>HTML y CSS - Horario</title>
+    <link rel="stylesheet" href="estilosHorario.css">
+</head>
+
+<body>
+    <div class="cabecera">
+
+        @yield("cabecera")
+
+    </div>
+
+    <div class="tabla">
+
+        @yield("tabla")
+
+    </div>
+</body>
+</html>
